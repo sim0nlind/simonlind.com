8b133d65693ed999d5d5ec483bc5bd777393246d
Simon Lind <simonlind@robotbeats.dk>
2024-08-07 10:00:34 +0200

    Dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import {
   Links,
   Meta,
@@ -6,11 +7,39 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import Nav from "./components/nav/nav";
+
 import "./global.css";
 import "./tailwind.css";
 import styles from "./root.module.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // Check if the user has dark mode enabled
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
+
+    // Function to update the body class based on the media query
+    const updateBodyClass = (e: MediaQueryListEvent) => {
+      if (e.matches) {
+        document.body.classList.add("dark");
+      } else {
+        document.body.classList.remove("dark");
+      }
+    };
+
+    // Initial check
+    updateBodyClass(darkModeMediaQuery);
+
+    // Add event listener for changes in the media query
+    darkModeMediaQuery.addEventListener("change", updateBodyClass);
+
+    // Cleanup listener on component unmount
+    return () => {
+      darkModeMediaQuery.removeEventListener("change", updateBodyClass);
+    };
+  }, []);
+
   return (
     <html lang="en">
       <head>
