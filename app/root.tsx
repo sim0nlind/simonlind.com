190cbe3e0f65e99b71904d07409500187c638e89
Simon Lind <simonlind@robotbeats.dk>
2024-08-09 11:15:57 +0200

    Proper font

--- app/root.tsx
+++ app/root.tsx
@@ -6,40 +6,22 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import Nav from "./components/nav/nav";
+import type { LinksFunction } from "@remix-run/node";
 
+import Nav from "./components/nav/nav";
 import "./global.css";
 import "./tailwind.css";
 import styles from "./root.module.css";
 
+export const links: LinksFunction = () => [
+  {
+    rel: "preload",
+    href: "./assets/fonts/Geist-Variable.woff2",
+    type: "font/woff2",
+  },
+];
+
 export function Layout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    // Check if the user has dark mode enabled
-    const darkModeMediaQuery = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    );
-
-    // Function to update the body class based on the media query
-    const updateBodyClass = (e: MediaQueryListEvent) => {
-      if (e.matches) {
-        document.body.classList.add("dark");
-      } else {
-        document.body.classList.remove("dark");
-      }
-    };
-
-    // Initial check
-    updateBodyClass(darkModeMediaQuery);
-
-    // Add event listener for changes in the media query
-    darkModeMediaQuery.addEventListener("change", updateBodyClass);
-
-    // Cleanup listener on component unmount
-    return () => {
-      darkModeMediaQuery.removeEventListener("change", updateBodyClass);
-    };
-  }, []);
-
   return (
     <html lang="en">
       <head>
