3f480e689d5b4b35a89cf1e018d93d102784cc48
Simon Lind <simonlind@robotbeats.dk>
2024-08-14 23:13:56 +0200

    Minor fixes

--- app/components/workExperience/workExperience.tsx
+++ app/components/workExperience/workExperience.tsx
@@ -52,27 +52,32 @@ export default function WorkExperience(props: WorkExperienceProps) {
       </div>
       <p className={styles.description}>{props.description}</p>
       {props.images && (
-        <div className={styles.carousel}>
-          <div className={styles.carouselInner}>
-            {props.images.map((image) => (
-              <img
-                key={image.index}
-                id={"image-" + image.index}
-                src={image.src}
-                alt={`Image ${image.index}`}
-                ref={(el) => (imageRefs.current[image.index] = el)}
-                onClick={() =>
-                  handleImageClick(image.index, imageRefs.current[image.index])
-                }
-              />
-            ))}
-            <div className={styles.spacer} />
+        <>
+          <div className={styles.carousel}>
+            <div className={styles.carouselInner}>
+              {props.images.map((image) => (
+                <img
+                  key={image.index}
+                  id={"image-" + image.index}
+                  src={image.src}
+                  alt={`Image ${image.index}`}
+                  ref={(el) => (imageRefs.current[image.index] = el)}
+                  onClick={() =>
+                    handleImageClick(
+                      image.index,
+                      imageRefs.current[image.index]
+                    )
+                  }
+                />
+              ))}
+              <div className={styles.spacer} />
+            </div>
           </div>
-        </div>
+          <button onClick={handlePrevImage}>Prev</button>
+          <button onClick={handleNextImage}>Next</button>
+          <p>{activeImage}</p>
+        </>
       )}
-      <button onClick={handlePrevImage}>Prev</button>
-      <button onClick={handleNextImage}>Next</button>
-      <p>{activeImage}</p>
     </div>
   );
 }
