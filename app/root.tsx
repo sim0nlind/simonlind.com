e4f08237900f56eaf0814c2cbe6a67772cd00bcd
Simon Lind <simonlind@robotbeats.dk>
2024-08-28 21:56:38 +0200

    Analyticsss

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import { Analytics } from "@vercel/analytics/react";
 
 import Nav from "./components/nav/nav";
 import "./global.css";
@@ -40,6 +41,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
             {children}
             <ScrollRestoration />
             <Scripts />
+            <Analytics />
           </div>
         </main>
       </body>
