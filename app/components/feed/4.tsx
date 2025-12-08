a83ff068f18f58a60f37b4ccb7b8b973bff81b33
Simon Lind <simonlindhansen91@gmail.com>
2025-12-08 22:11:48 +0100

    mp4 is better

--- app/components/feed/4.tsx
+++ app/components/feed/4.tsx
@@ -1,9 +1,9 @@
-import vid4 from "../../assets/feed/4.webm";
+import vid4 from "../../assets/feed/4.mp4";
 
 export default function feed4() {
   return (
     <video width="1920" height="1080" autoPlay muted loop>
-      <source src={vid4} type="video/webm" />
+      <source src={vid4} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
