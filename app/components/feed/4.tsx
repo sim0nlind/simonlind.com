f9f945d95ad3b9255f8e1bc3d02e3f469e57d754
Simon Lind <simonlindhansen91@gmail.com>
2025-12-09 09:33:07 +0100

    better mobile support for videos

--- app/components/feed/4.tsx
+++ app/components/feed/4.tsx
@@ -2,7 +2,7 @@ import vid4 from "../../assets/feed/4.mp4";
 
 export default function feed4() {
   return (
-    <video width="1920" height="1080" autoPlay muted loop>
+    <video width="1920" height="1080" autoPlay playsInline muted loop preload="auto">
       <source src={vid4} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
--- app/components/feed/7.tsx
+++ app/components/feed/7.tsx
@@ -2,7 +2,7 @@ import vid7 from "../../assets/feed/7.mp4";
 
 export default function feed7() {
   return (
-    <video width="1920" height="1080" autoPlay muted loop>
+    <video width="2000" height="1126" autoPlay playsInline muted loop preload="auto">
       <source src={vid7} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
